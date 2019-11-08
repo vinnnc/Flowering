a4b658b57c71c7d5931ff36871cc849c87d4b7ec
Wenchu Du <duwenchu999@gmail.com>
2019-11-08 13:11:10 +1100

    Add data model and generate plant entity class

---
+++ Plant+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Plant+CoreDataClass.swift
+//  Flowering
+//
+//  Created by Wenchu Du on 8/11/19.
+//  Copyright © 2019 Personal. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Plant)
+public class Plant: NSManagedObject {
+
+}
